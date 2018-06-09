7b23d3a536653487a30d625f81c152942b39eea1
jojoldu <jojoldu@gmail.com>
2018-06-09 18:49:07 +0900

    텔래그램 봇 기본 기능 완성!

--- lambda.js
+++ lambda.js
@@ -8,29 +8,46 @@ const TOKEN = process.env.TOKEN;
 const JSON_URL = process.env.JSON_URL;
 const https = require('https');
 const util = require('util');
+const { StringDecoder } = require('string_decoder');
+const decoder = new StringDecoder('utf8');
 
 const commands = [
     {"path": "/start", "description":"시작 및 도움말"},
-    {"path": "/list", "description":"채용 목록"},
-    {"path": "/start", "description":"시작 및 도움말"},
-    {"path": "/start", "description":"시작 및 도움말"}
+    {"path": "/recruits", "description":"현재 채용 목록"},
+    {"path": "/github", "description":"저장소 Github Link"},
+    {"path": "/facebook", "description":"페이스북 Link"},
 ];
 
 exports.handler = (event, context) => {
-    const requestChatId = event.message.from.id;
+    console.log(JSON.stringify(event));
+    const requestChatId = event.message.chat.id;
     const requestText = event.message.text;
 
-    if(requestText === "/list"){
-        https.request(JSON_URL, (res)=>{
-            console.log(res);
-            const recruits = JSON.parse(res);
-            sendMessage(context, requestChatId, JSON.stringify(recruits));
+    if(requestText === "/start" || requestText === "/start@devbeginner_bot") {
+        const intro = "초보개발자모임 Bot Commands\n";
+        const message = commands.map( c => c.path +" - " + c.description).join("\n");
+        sendMessage(context, requestChatId, intro+message);
+    } else if(requestText === "/recruits"|| requestText === "/recruits@devbeginner_bot") {
+        https.get(JSON_URL, (res) => {
+            res.on('data', (d) => {
+                const strJson = decoder.write(d);
+                sendMessage(context, requestChatId, toMessage(JSON.parse(strJson).recruits));
+            });
         });
+    } else if(requestText === "/github"|| requestText === "/github@devbeginner_bot"){
+        sendMessage(context, requestChatId, "http://bit.ly/2JpiOs7");
+    } else if(requestText === "/facebook"|| requestText === "/facebook@devbeginner_bot"){
+        sendMessage(context, requestChatId, "http://bit.ly/2HBXkSW");
     } else {
-        sendMessage(context, requestChatId, "없는 명령어 입니다.");
+        console.log("없는 명령어 입니다. "+ requestText);
     }
 };
 
+function toMessage(recruits) {
+    return recruits
+        .map( (r) => "[" + r.endDate + "] " +r.description + " ("+r.link+")")
+        .join("\n")
+}
 function sendMessage(context, chatId, text) {
     const postData = {
         "chat_id": chatId,
