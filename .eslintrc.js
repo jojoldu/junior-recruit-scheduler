a14cdef363d50b26a5f812311efbd148f5c665a6
jojoldu <jojoldu@gmail.com>
2022-03-27 17:41:44 +0900

    eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: 'tsconfig.json',
     sourceType: 'module',
     extraFileExtensions: [
